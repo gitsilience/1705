1ad9be82682a8842504684ecd08b14b3be79870c
blue .EVE <blue .EVE>
2017-09-21 22:43:49 +0800

    测试

---
+++ NO.1onClass/app.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Created by w on 2017/9/15.
+ */
+var a = 20;
+var b = 30;
+function sortName(list) {
+}
---
+++ NO.1onClass/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by w on 2017/9/15.
+ */
+var a = 20;
+let  b = 30;
+
+function sortName(list:any){
+
+}
